65fc92a77305e72ec877a0383c62fbc7755f49a9
root <root@vmgit.rwqb104qs5zu1e2ukk11yezxga.ax.internal.cloudapp.net>
2023-05-20 09:43:41 +0000

    zmiany

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const port = process.env.PORT || 3000;
 
 app.listener(port, () =>
-	console.log('Aplikacja nasluchuje na porcie 3000');
+	console.log('Aplikacja nasluchuje na porcie 3000'),
 );
